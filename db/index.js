94e11c6b30800691c2b54b5281a8f7843d1a9263
ziggyboy2001 <brentpurks1@icloud.com>
2023-03-03 09:19:47 -0600

    juiceBox finished with step three

--- db/index.js
+++ db/index.js
@@ -31,6 +31,25 @@ async function getAllPosts() {
   }
 }
 
+async function getPostsByTagName(tagName) {
+  try {
+    const { rows: postIds } = await client.query(
+      `
+        SELECT posts.id
+        FROM posts
+        JOIN post_tags ON posts.id=post_tags."postId"
+        JOIN tags ON tags.id=post_tags."tagId"
+        WHERE tags.name=$1;
+        `,
+      [tagName]
+    );
+
+    return await Promise.all(postIds.map((post) => getPostById(post.id)));
+  } catch (error) {
+    throw error;
+  }
+}
+
 async function createUser({ username, password, name, location }) {
   try {
     const { rows } = await client.query(
@@ -98,28 +117,54 @@ async function updateUser(id, fields = {}) {
   }
 }
 
-async function updatePost(id, field = { title, content, active }) {
-  const setString = Object.keys(field)
+async function updatePost(postId, fields = {}) {
+  // read off the tags & remove that field
+  const { tags } = fields; // might be undefined
+  delete fields.tags;
+
+  // build the set string
+  const setString = Object.keys(fields)
     .map((key, index) => `"${key}"=$${index + 1}`)
     .join(", ");
-  // return early if this is called without fields
-  if (setString.length === 0) {
-    return;
-  }
+
   try {
-    const {
-      rows: [post],
-    } = await client.query(
+    // update any fields that need to be updated
+    if (setString.length > 0) {
+      await client.query(
+        `
+          UPDATE posts
+          SET ${setString}
+          WHERE id=${postId}
+          RETURNING *;
+        `,
+        Object.values(fields)
+      );
+    }
+
+    // return early if there's no tags to update
+    if (tags === undefined) {
+      return await getPostById(postId);
+    }
+
+    // make any new tags that need to be made
+    const tagList = await createTags(tags);
+    const tagListIdString = tagList.map((tag) => `${tag.id}`).join(", ");
+
+    // delete any post_tags from the database which aren't in that tagList
+    await client.query(
       `
-        UPDATE posts
-        SET ${setString}
-        WHERE "authorId"=${id}
-        RETURNING *;
+        DELETE FROM post_tags
+        WHERE "tagId"
+        NOT IN (${tagListIdString})
+        AND "postId"=$1;
       `,
-      Object.values(field)
+      [postId]
     );
 
-    return post;
+    // and create post_tags as necessary
+    await addTagsToPost(postId, tagList);
+
+    return await getPostById(postId);
   } catch (error) {
     throw error;
   }
@@ -292,4 +337,5 @@ module.exports = {
   createTags,
   createPostTag,
   addTagsToPost,
+  getPostsByTagName,
 };
