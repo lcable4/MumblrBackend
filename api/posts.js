20a89516bc4aec635dba9e0cbf4cb8d61f3795f3
Logan <logancable@ymail.com>
2023-03-03 14:23:16 -0800

    writing PATCH api posts

--- api/posts.js
+++ api/posts.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { getAllPosts } = require('../db');
 const postsRouter = express.Router();
+const { requireUser } = require('./utils');
+const { createPost } = require('../db');
 
 postsRouter.use((req, res, next) => {
     console.log("A request is being made to /posts");
@@ -15,5 +17,28 @@ postsRouter.use((req, res, next) => {
           posts
       });
   });
+
+  postsRouter.post('/', requireUser, async (req, res, next) => {
+    const { title, content, tags = "" } = req.body;
+    
+    const tagArr = tags.trim().split(/\s+/);
+    const postData = {
+        authorId: req.user.id,
+        title,
+        content,
+        tags: tagArr,
+    };
+    
+    try {
+    const post = await createPost(postData);
+    if (post) {
+        res.send({ post });
+        } else {
+        next({ name: 'PostCreationError', message: 'Error creating post.' });
+        }
+        } catch ({ name, message }) {
+        next({ name, message });
+    }
+    });
 
 module.exports = postsRouter;
