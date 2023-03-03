34251238e067e0febc25e4aae48251aa8ed00951
Logan <logancable@ymail.com>
2023-03-03 11:00:00 -0800

    working on API routing

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const postsRouter = require('./posts');
 const apiRouter = express.Router();
-
 const usersRouter = require('./users');
-apiRouter.use('./users', usersRouter);
+
+apiRouter.use('/users', usersRouter);
+apiRouter.use('/posts', postsRouter);
+
 
 module.exports = apiRouter;
 
