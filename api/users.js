beadb5908003f627971bee6ffc0667c042c0404e
ziggyboy2001 <brentpurks1@icloud.com>
2023-03-03 13:44:09 -0600

    finished jwt, moving into fiunal step

--- api/users.js
+++ api/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const usersRouter = express.Router();
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = process.env;
-const { getAllUsers, getUserByUsername } = require("../db");
+const { getAllUsers, getUserByUsername, createUser } = require("../db");
 
 usersRouter.use((req, res, next) => {
   console.log("A request is being made to /users");
@@ -29,15 +29,16 @@ usersRouter.post("/login", async (req, res, next) => {
   }
 
   try {
+    const user = await getUserByUsername(username);
 
-      
-      const user = await getUserByUsername(username);
-      
-      if (user && user.password == password) {
-        const token = jwt.sign({
-            id: user.id,
-            username
-        }, process.env.JWT_SECRET);
+    if (user && user.password == password) {
+      const token = jwt.sign(
+        {
+          id: user.id,
+          username,
+        },
+        process.env.JWT_SECRET
+      );
       res.send({ message: "You are logged in!", token });
     } else {
       next({
@@ -48,6 +49,46 @@ usersRouter.post("/login", async (req, res, next) => {
   } catch (error) {
     console.log(error);
     next(error);
+  }
+});
+
+usersRouter.post("/register", async (req, res, next) => {
+  const { username, password, name, location } = req.body;
+
+  try {
+    const _user = await getUserByUsername(username);
+
+    if (_user) {
+      next({
+        name: "UserExistsError",
+        message: "A user by that username already exists",
+      });
+    }
+
+    const user = await createUser({
+      username,
+      password,
+      name,
+      location,
+    });
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        username,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1w",
+      }
+    );
+
+    res.send({
+      message: "thank you for signing up",
+      token,
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
   }
 });
 
